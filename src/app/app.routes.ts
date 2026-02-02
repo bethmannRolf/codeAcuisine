b2b85e124dcc714b79db250aff4ddf128c52d9d5
bethmannRolf <devak.robe@gmail.com>
2026-02-02 11:46:08 +0100

    added new route to cookbook in routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { GenerateRecipePageComponent } from './generate-recipe-page/generate-recipe-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+    {path:'', component:LandingPageComponent},
+    {path: 'cookbook', component: GenerateRecipePageComponent}
+
+
+];
