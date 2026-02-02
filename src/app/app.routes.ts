9d24cdfe41b2bf25f0346e79ba1809ea2d230115
bethmannRolf <devak.robe@gmail.com>
2026-02-02 19:08:16 +0100

    router cookbook integrated

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,12 +2,15 @@ import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { GenerateRecipePageComponent } from './generate-recipe-page/generate-recipe-page.component';
+import { CookbookCountryBoxComponent } from './cookbook-country-box/cookbook-country-box.component';
+import { CountryPageComponent } from './country-page/country-page.component';
+import { CookbookComponent } from './cookbook/cookbook.component';
 
 export const routes: Routes = [
 
     {path:'', component:LandingPageComponent},
     {path: 'recipeGenerator', component: GenerateRecipePageComponent},
-     {path: 'cookbook', component: GenerateRecipePageComponent}
+     {path: 'cookbook', component: CookbookComponent}
 
 
 ];
---
+++ src/app/cookbook/cookbook.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { HeaderTopGreenComponent } from "../header-top-green/header-top-green.component";
+import { CookbookHeadingBannerComponent } from "../cookbook-heading-banner/cookbook-heading-banner.component";
+
+@Component({
+  selector: 'app-cookbook',
+  imports: [HeaderTopGreenComponent, CookbookHeadingBannerComponent],
+  templateUrl: './cookbook.component.html',
+  styleUrl: './cookbook.component.scss',
+})
+
+
+
+export class CookbookComponent {
+
+
+
+
+
+
+
+}
