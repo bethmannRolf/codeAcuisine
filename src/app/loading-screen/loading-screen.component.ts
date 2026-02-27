f4adef936fa5ebfa21ad94119eb8a3c359c6afe0
bethmannRolf <devak.robe@gmail.com>
2026-02-27 13:19:50 +0100

    loading spinner finished

--- src/app/loading-screen/loading-screen.component.ts
+++ src/app/loading-screen/loading-screen.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderTopComponent } from "../header-top/header-top.component";
+import { LoadingSpinnerComponent } from "../loading-spinner/loading-spinner.component";
 
 @Component({
   selector: 'app-loading-screen',
-  imports: [HeaderTopComponent],
+  imports: [HeaderTopComponent, LoadingSpinnerComponent],
   templateUrl: './loading-screen.component.html',
   styleUrl: './loading-screen.component.scss',
 })
