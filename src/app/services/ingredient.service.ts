cf1e91b27c4be1172417716cfc4bcb9f16071dc8
bethmannRolf <devak.robe@gmail.com>
2026-03-03 15:54:12 +0100

    added json ingredient list

--- src/app/services/ingredient.service.ts
+++ src/app/services/ingredient.service.ts
@@ -1,40 +1,12 @@
 import { Injectable, signal } from '@angular/core';
+import ingredientsData from '../../assets/data/ingredients.json';
 
 @Injectable({
   providedIn: 'root'
 })
 export class IngredientService {
 
-  private _ingredients = signal<string[]>([
-    'Tomate',
-    'Mozzarella',
-    'Basilikum',
-    'Knoblauch',
-    'Olivenöl',
-    'Zwiebel',
-    'Paprika',
-    'Champignons',
-    'Spaghetti',
-    'Parmesan',
-    'Karotte',
-    'Brokkoli',
-    'Blumenkohl',
-    'Reis',
-    'Hähnchen',
-    'Rindfleisch',
-    'Lachs',
-    'Thunfisch',
-    'Ei',
-    'Milch',
-    'Butter',
-    'Sahne',
-    'Zucker',
-    'Salz',
-    'Pfeffer',
-    'Oregano',
-    'Thymian',
-    'Rosmarin'
-  ]);
+  private _ingredients = signal<string[]>(ingredientsData);
 
   ingredients = this._ingredients.asReadonly();
 }
